fbe2d03e748f8721ab439d2d70d598dd69a308d0
Oury <mamadou.barry@student.vinci.be>
2024-04-18 11:13:12 +0200

    setup du projet

---
+++ network.h
@@ -0,0 +1,23 @@
+/**
+ * PRE: serverIP : a valid IP address
+ *      serverPort: a valid port number
+ * POST: on success, connects a client socket to serverIP:serverPort ;
+ *       on failure, displays error cause and quits the program
+ * RES: return socket file descriptor
+ */
+int initSocketClient(char *serverIP, int serverPort);
+
+/**
+ * PRE: 
+ * 
+ * POST: disconnect  all the players in the table given as parameter
+ */
+void disconnect_players(Player *tabPlayers, int nbPlayers);
+
+/**
+ * PRE:  serverPort: a valid port number
+ * POST: on success, binds a socket to 0.0.0.0:serverPort and listens to it ;
+ *       on failure, displays error cause and quits the program
+ * RES:  return socket file descriptor
+ */
+int initSocketServer(int port);
